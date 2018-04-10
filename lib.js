1d6215f6487a891e901e0f26021344b557adda89
912187345 <912187345@qq.com>
2018-04-10 16:51:35 +0800

    增加lib 第三方模块

---
+++ lib.js
@@ -0,0 +1,12 @@
+module.exports = {
+    moment:require('moment'),
+    mysql:require('mysql'),
+    fs:require('fs'),
+    bodyParser:require('body-parser'),
+    cookieParser:require('cookie-parser'),
+    session:require('express-session'),
+    uuid:require('node-uuid'),
+    cheerio:require('cheerio'),
+    images:require('images'),
+    multiparty:require('multiparty'),
+};
